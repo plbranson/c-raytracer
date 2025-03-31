21a1461ebc5eccdd23160a56b2a7d8b6ddc85d5e
plbranson <plbranson@hotmail.com>
2025-03-31 10:31:11 -0400

    Added ./src/vector.c and ./include/vector.h

---
+++ include/vector.h
@@ -0,0 +1,44 @@
+/**
+ *  Copyright 2025 Patrick L. Branson
+ * 
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *  
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+
+#ifndef VECTOR_H
+#define VECTOR_H
+
+#include <math.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+#define X 0
+#define Y 1
+#define Z 2
+#define W 3
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+typedef double Vector[3];
+
+
+void vector_init(Vector dst, double x, double y, double z);
+
+void vector_copy(Vector dst, Vector src);
+
+#ifdef __cplusplus
+} // C++ Wrapper
+#endif
+
+#endif /* VECTOR_H */
